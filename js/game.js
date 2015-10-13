6e9ed22fea5999f260c9120575a702778ef0d49a
Jay Kim <sinefinehabitarevolo@gmail.com>
2015-10-13 00:07:52 -0400

    Update

--- js/game.js
+++ js/game.js
@@ -1,3 +1,15 @@
+window.requestAnimFrame = (function(callback) {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+        function(callback) {
+          window.setTimeout(callback, 1000 / 60);
+        };
+      })();
+
+  var blue = '#3A5BCD';
+  var red = '#EF2B36';
+  var yellow = '#FFC636';
+  var green = '#02A817';
+
 function initBalls(x, y, vx, vy, color) {
   balls = [];
   balls.push(new Ball(x, y, vx, vy, color));
@@ -26,64 +38,52 @@ function getMousePos(canvas, evt) {
 
 function updateBalls(canvas, balls, timeDiff, mousePos) {
   var context = canvas.getContext('2d');
-  var collisionDamper = 1;
-  var floorFriction = 0.0005 * timeDiff;
-  var mouseForceMultiplier = 1 * timeDiff;
-  var restoreForce = 0.002 * timeDiff;
+  
 
-  for(var n = 0; n < balls.length; ++n) {
+  for(var n = 0; n < balls.length; n++) {
     var ball = balls[n];
-    // set ball position based on velocity
-    ball.y += ball.vy;
-    ball.x += ball.vx;
 
     if(ball.x > canvas.width || ball.y > canvas.height || ball.x < 0 || ball.y < 0){
       balls.splice(n, 1);
-      balls.push(new Ball(200, 60, 0, 0, blue));
-
-    };
-
-    // mouse forces
-    var mouseX = mousePos.x;
-    var mouseY = mousePos.y;
-
-    var distX = ball.x - mouseX;
-    var distY = ball.y - mouseY;
-
-    var radius = Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2));
-
-    var totalDist = Math.abs(distX) + Math.abs(distY);
-
-    var forceX = (Math.abs(distX) / totalDist) * (1 / radius) * mouseForceMultiplier;
-    var forceY = (Math.abs(distY) / totalDist) * (1 / radius) * mouseForceMultiplier;
-
-    if(distX > 0) {// mouse is left of ball
-      ball.vx += forceX;
-    }
-    else {
-      ball.vx -= forceX;
-    }
-    if(distY > 0) {// mouse is on top of ball
-      ball.vy += forceY;
-    }
-    else {
-      ball.vy -= forceY;
+      balls.push(new Ball(500, 300, 0, 0, blue));
     }
 
-    // floor friction
-    if(ball.vx > 0) {
-      ball.vx -= floorFriction;
-    }
-    else if(ball.vx < 0) {
-      ball.vx += floorFriction;
-    }
-    if(ball.vy > 0) {
-      ball.vy -= floorFriction;
-    }
-    else if(ball.vy < 0) {
-      ball.vy += floorFriction;
-    }
+    radius = 20
 
+    distanceBallandMouse = Math.sqrt(Math.pow((mousePos.currX-ball.x),2)+Math.pow((mousePos.currY-ball.y),2));
+
+    var angle = 0;
+    if (distanceBallandMouse < 20){
+
+      var dirX = mousePos.currX-ball.x;
+      var dirY = mousePos.currY-ball.y;
+      angle = Math.atan(Math.abs(dirY)/Math.abs(dirX));
+
+      if (dirX > 0){
+        ball.vx = -Math.cos(angle)*mouseVelMag;
+      }
+      else {
+        ball.vx = Math.cos(angle)*mouseVelMag;
+      }
+
+      if (dirY > 0){
+        ball.vy = -Math.sin(angle)*mouseVelMag;  
+      }
+      else {
+        ball.vy = Math.sin(angle)*mouseVelMag;
+      }
+      // console.log("angle: " + angle);
+      // console.log("-----------------");
+      // console.log("mousevel.x: " + mouseVel.x);
+      // console.log("mousevel.y: " + mouseVel.y);
+      // console.log("-----------------");
+      // console.log("ball.x: " + ball.vx);
+      // console.log("ball.y: " + ball.vy); 
+      // console.log("-----------------");
+   
+    }
+      ball.x += ball.vx;
+      ball.y += ball.vy;
   }
 }
 
@@ -95,14 +95,24 @@ function Ball(x, y, vx, vy, color) {
   this.color = color;
   this.origX = x;
   this.origY = y;
-  this.radius = 10;
+  this.radius = 20;
 }
 
-function clearscreen(){
+function animate(canvas, balls, lastTime, mousePos) {
+  var context = canvas.getContext('2d');
+
+  //time check
+  date = new Date();
+  time = date.getTime();
+  timeDiff = time - lastTime;
+  lastTime = time;
+  
+  //mouse 
+  updateBalls(canvas, balls, timeDiff, mousePos);
+  // clear
   context.clearRect(0, 0, canvas.width, canvas.height);
-}
 
-function draw(){
+  // render
   for(var n = 0; n < balls.length; n++) {
     var ball = balls[n];
     context.beginPath();
@@ -110,58 +120,64 @@ function draw(){
     context.fillStyle = ball.color;
     context.fill();
   }
+
+  // request new frame
+  requestAnimFrame(function() {
+    animate(canvas, balls, lastTime, mousePos);
+  });
 }
 
-function animate() {
-  // update
-  var date = new Date();
-  var newtime = date.getTime();
-  var timeDiff = newtime - time;
-  time = newtime;
+var mouseMass = 10;
+var ballMass = 5;
+var canvas = document.getElementById('canvas');
+var balls = initBalls(500, 300,0,0,blue);
+var date = new Date();
+var time = date.getTime();
+var timeDiff = 0;
+var lastTime = time;
+
+var mouseCount = 0;
+
+var mouseVel = {
+  x: 0,
+  y: 0
+}
+
+var mouseVelMag = 0;
+
+var mousePos = {
+  currX : 9999,
+  currY : 9999,
+  prevX : 9999,
+  prevY : 9999
+};
 
 canvas.addEventListener('mousemove', function(evt) {
   var pos = getMousePos(canvas, evt);
-  mousePos.x = pos.x;
-  mousePos.y = pos.y;
+  mousePos.currX = pos.x;
+  mousePos.currY = pos.y;
+  time = date.getTime();
+  timeDiff = time - lastTime;    // right now need to reduce the refresh rate. 
+  lastTime = time;
+  var stepsize = 10;
+  if(timeDiff > stepsize){
+    mouseVel.x += ((mousePos.currX - mousePos.prevX) / timeDiff * stepsize);
+    mouseVel.y += ((mousePos.currY - mousePos.prevY) / timeDiff * stepsize);
+    mouseCount++; 
+    if (mouseCount > stepsize){ 
+    	mouseVel.x = mouseVel.x/stepsize;
+    	mouseVel.y = mouseVel.y/stepsize;
+      mouseCount = 0;
+    }
+  }
+  mouseVelMag = Math.sqrt(Math.pow(mouseVel.x, 2)+Math.pow(mouseVel.y, 2));
+  mousePos.prevX = mousePos.currX;
+  mousePos.prevY = mousePos.currY;
 });
-  
-  updateBalls(canvas, balls, timeDiff, mousePos);
-  clearscreen();
-  draw();
-
-  // request new frame. recursive function cooool
-  if (window.webkitRequestAnimationFrame !== undefined) {
-      window.webkitRequestAnimationFrame(animate);
-   }
-   else if (window.mozRequestAnimationFrame !== undefined) {
-      window.mozRequestAnimationFrame(animate);
-   }
-}
-
-var blue = '#3A5BCD';
-var red = '#EF2B36';
-var yellow = '#FFC636';
-var green = '#02A817';
-
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var balls = initBalls(173,60,0,0,blue);
-var date = new Date();
-var time = date.getTime();
-var mousePos = {
-  x: 9999,
-  y: 9999
-};
-
-// canvas.addEventListener('mousemove', function(evt) {
-//   var pos = getMousePos(canvas, evt);
-//   mousePos.x = pos.x;
-//   mousePos.y = pos.y;
-// });
 
 canvas.addEventListener('mouseout', function(evt) {
-  mousePos.x = 9999;
-  mousePos.y = 9999;
+  mouseVel.x = 0;
+  mouseVel.y = 0;
 });
 
-animate();
+animate(canvas, balls, time, mousePos);
